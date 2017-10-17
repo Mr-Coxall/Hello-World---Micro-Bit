287c1e2e2c886b3a014e806bce2f739221ca064c
Patrick Coxall <mr.coxall@mths.ca>
2017-10-16 22:49:46 -0400

    Init

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# Created by: Mr. Coxall
+# Created on: Oct 2017
+# Created for: ICS3U
+# This program shows "Hello World!" on a Micro:Bit
+
+from microbit import *
+
+while True:
+    display.scroll('Hello, World!')
+    sleep(2000)
